561552ef3e8feca92b335a2ba29d1bf1d0a66eef
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2024-07-09 03:29:41 -0230

    condtional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
+import Todo from "./todo";
 function App() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Student></Student>
+      <Todo task="Learn React" isDone={true}></Todo>
+      <Todo task="Explore concepts" isDone={true}></Todo>
+      <Todo task="Try JSX" isDone={false}></Todo>
+      <Todo task="Try JSX" isDone={false}></Todo>
     </>
   );
+}
+
+function Student(props) {
+  console.log(props);
+  return <h2>i am a Student</h2>;
 }
 
 export default App;
---
+++ src/todo.jsx
@@ -0,0 +1,16 @@
+export default function Todo({ task, isDone }) {
+  //   return (
+  //      <li>Task: {task}</li>
+  //   )
+  // conditional rendering
+  //   if (isDone === true) {
+  //     return <li>Finished: {task}</li>;
+  //   } else {
+  //     return <li>Work on: {task}</li>;
+  //   }
+
+  if (isDone) {
+    return <li>Finish: {task}</li>;
+  }
+  return <li>working on: {task}</li>;
+}
