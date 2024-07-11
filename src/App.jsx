a5b5698fb8452cf9251d78d0c9c97b9669824afa
Kazi Salith Ur Rahman <salith.rahman107@gmail.com>
2024-07-11 14:52:35 -0230

    mapping

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import "./App.css";
 import Todo from "./todo";
+import Actor from "./Actor";
 function App() {
+  const actors = ["Sakib", "raj", "Jasim", "rubel", "shah"];
   return (
     <>
       <h1>Vite + React</h1>
       <Student></Student>
+      <Actor name={"Salman"}></Actor>
+      {actors.map((actor) => (
+        <Actor name={actor}></Actor>
+      ))}
       <Todo task="Learn React" isDone={true}></Todo>
       <Todo task="Explore concepts" isDone={true}></Todo>
       <Todo task="Try JSX" isDone={false}></Todo>
